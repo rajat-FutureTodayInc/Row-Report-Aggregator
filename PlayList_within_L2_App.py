978c9b22f8d5dd29437eafc53e5968a20abacec2
ANANDSM016 <anandshuklapa@gmail.com>
2024-07-22 11:25:51 +0530

    Update PlayList_within_L2_App.py

--- PlayList_within_L2_App.py
+++ PlayList_within_L2_App.py
@@ -12,12 +12,15 @@ from io import BytesIO
 # Define the PlayList_within_L2 function
 def PlayList_within_L2(raw_data, L1, L2):
     data = raw_data.copy()
-    data = data.iloc[:, :21]  # Keeping the first 21 columns
-    
+    data = data.iloc[:, :21]  # Removing the Node related columns as they generate duplicates
+
+    #Applying Filters
     mask1 = (data['L1 Category'] == L1) | (L1 == "All")
     mask2 = (data['L2 Category'] == L2) | (L2 == "All")
     data1 = data[mask1 & mask2]
 
+
+    #Generating the latest position columns corresponding to given PlayLists
     data2 = data1.copy()
     data3 = data2.iloc[:, [11, 13]]         #not droping the mlp column, hence 11 and 13 (if dropped 5, 7)
     data3 = data3.dropna(subset=['Play ListID'])
@@ -29,26 +32,36 @@ def PlayList_within_L2(raw_data, L1, L2):
         play_list_id = data3.at[i, 'Play ListID']
         df1 = data2[data2['Play ListID'] == play_list_id].sort_values(by='Date', ascending=False).reset_index()
         data3.at[i, 'Play List Position'] = df1.at[0, 'Play List Position']
-    
+
+
+    #Converting watch time from sec to hrs
     data1 = data1.assign(Watch_Time = (data1['Watch Time']/3600))
-    data1 = data1.drop_duplicates()
-    
+
+    #Removing duplicates generated due to node related columns 
+    data1 = data1.drop_duplicates()     
+
+    #Modifying the indexes on which grouping is to be done
     index_columns = ['L1 Category', 'L2 Category', 'Play ListID']
     if L1 == 'All':
         index_columns.remove('L1 Category')
     if L2 == 'All':
         index_columns.remove('L2 Category')
 
+
+    #Creating the required pivot table
     piv_data = pd.pivot_table(
         data1,
         values=['Unique Users', 'Watch_Time', 'Views'],
         index=index_columns,
         aggfunc={'Unique Users': 'sum', 'Watch_Time': 'sum', 'Views':'sum'}
     )
-        
+
+    #Handling the case for empty pivot table
     if(piv_data.shape[0] == 0):
         return piv_data
-    
+
+
+    #Adding the PlayList names column corresponding to the PlayListId
     play_list_dict = data1.drop_duplicates(subset = ['Play ListID']).set_index('Play ListID')['Play List'].to_dict()
     piv_data = piv_data.reset_index()
     piv_data = piv_data.assign(Play_List = piv_data['Play ListID'].map(play_list_dict))  
@@ -56,15 +69,19 @@ def PlayList_within_L2(raw_data, L1, L2):
     cols.insert(piv_data.shape[1]-4, cols.pop(cols.index('Play_List')))
     piv_data = piv_data[cols]
 
+
+    #Assigning wt/uu
     piv_data = piv_data.assign(wt_uu=(piv_data['Watch_Time'] / piv_data['Unique Users']) * 60)
     piv_data_reset = piv_data.reset_index()
 
+    #Merging the latest position dataframe
     df = pd.merge(piv_data_reset, data3, on='Play ListID', how='left')
+    
     df.drop(columns=['index'], inplace=True)
     
     return df
 
-
+#Function that returns an aggregated output excel file when user gives a raw_input excel file instead of manually selecting the categories
 def PlayList_in_L2(raw_data, raw_input):
     output = BytesIO()
     with pd.ExcelWriter(output, engine='openpyxl') as writer:
@@ -75,6 +92,8 @@ def PlayList_in_L2(raw_data, raw_input):
     output.seek(0)
     return output
 
+
+#Main function that generated a streamlit app that aggregates the PlayList data within a L2
 def main():
     # Streamlit app
     st.title("PlayList Aggregator")
